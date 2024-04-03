89e78ae41ccaeed48d6cda58e0489fbd1071f65c
Kamva Vanqa <kvanqa@com12.sdpdyn.kat.ac.za>
2024-04-03 15:34:45 +0200

    I have modified the condition to check for good files, I have removed the redundancy of chacking channels as the files are already 4k mode. I have added to check if the observation is not a snapshot(it must be atleast 1 hour long)

--- script/kathprfi_script.py
+++ script/kathprfi_script.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 import argparse
 import ast
 import logging
@@ -80,17 +80,17 @@ def main():
         counter = np.zeros((24, 4096, 2016, 8, 24), dtype=np.uint16)
         s = tme.time()
         logging.info('Adding file {} : {}'.format(i, f[i]))
+
         try:
             pathvis = f[i]
             vis = kathp.readfile(pathvis)
             logging.info('File number {} has been read'.format(i))
-            
-            if len(vis.freqs) == freq_chan and vis.dump_period > (dump_rate-1) and vis.dump_period <= dump_rate:
+            if vis.dump_period > (dump_rate-1) and vis.dump_period <= dump_rate and vis.vis.shape[0]>= dump_rate*dump_rate:     
                 logging.info('Removing bad antennas')
                 clean_ants = kathp.remove_bad_ants(vis)
                 logging.info('Bad antennas has been removed.')
                 good_flags = kathp.selection(vis, pol_to_use=pol, corrprod=corrpro, scan=scans,
-                                             clean_ants=clean_ants, flag_type=flags)
+                                                clean_ants=clean_ants, flag_type=flags)
                 logging.info('Good flags has been returned')
                 if good_flags.shape[0] * good_flags.shape[1] * good_flags.shape[2] != 0:
                     # Updating the array
@@ -114,17 +114,17 @@ def main():
                         El_idx = kathp.get_el_idx(el, elbins)[time_slice]
                         Az_idx = kathp.get_az_idx(az, azbins)[time_slice]
                         master, counter = kathp.update_arrays(Time_idx, Bl_idx, El_idx, Az_idx,
-                                                              flag_chunk, master, counter)
+                                                                flag_chunk, master, counter)
                     logging.info('{} s has been taken to update file number {}'.format(i,
-                                                                                       tme.time()
-                                                                                       - s))
+                                                                                        tme.time()
+                                                                                        - s))
                     goodfiles.append(f[i])
                     logging.info('Creating Xarray Dataset')
                     ds = xr.Dataset({'master': (('time', 'frequency', 'baseline', 'elevation',
-                                                 'azimuth'), master),
-                   'counter': (('time', 'frequency', 'baseline', 'elevation', 'azimuth'), counter)},
-                   {'time': np.arange(24), 'frequency': vis.freqs, 'baseline': np.arange(2016),
-                       'elevation': np.linspace(10, 80, 8), 'azimuth': np.arange(0, 360, 15)})
+                                                    'azimuth'), master),
+                    'counter': (('time', 'frequency', 'baseline', 'elevation', 'azimuth'), counter)},
+                    {'time': np.arange(24), 'frequency': vis.freqs, 'baseline': np.arange(2016),
+                        'elevation': np.linspace(10, 80, 8), 'azimuth': np.arange(0, 360, 15)})
                     logging.info('Saving dataset')
                     name, ext = os.path.splitext(args.zarr)
                     flname = name+str(f[i][46:56])+ext
@@ -139,9 +139,9 @@ def main():
                 badfiles.append(f[i])
                 pass
             np.save(args.good,goodfiles)
-            np.save(args.bad,badfiles)
+            np.save(args.bad, badfiles)
             logging.info('File has been saved')
-            
+        
 
         except Exception as e:
             logging.info(e)
